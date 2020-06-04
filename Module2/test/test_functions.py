5070f9651572c79218a83734a318dce0eae570c2
jefferydecker <jefferydecker@gmail.com>
2020-06-04 00:32:03 -0500

    add docstrings and constant

--- Module2/test/test_functions.py
+++ Module2/test/test_functions.py
@@ -1,3 +1,14 @@
+"""
+Program: test_functions.py
+Author: Jeffery Decker jefferydecker@gmail.com jmdecker1@dmacc.edu
+Last date modified: 06/04/2020
+
+
+The purpose of this program is to test the convert_to_months
+function from camper_age_input.py
+"""
+
+
 import unittest
 from main import camper_age_input
 from main.camper_age_input import convert_to_months
